85d0b70b5b76e49af3bb8a9c62860c0d81962768
PragneshKumar Singh <71432507+pragnesh-singh-rajput@users.noreply.github.com>
2025-12-16 15:56:53 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.sharencrypt.app',
-  appName: 'Sharencrypt',
-  webDir: 'dist'
-};
-
-export default config;
